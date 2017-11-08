e7f5b639520ecba920484aa0f6908c9bb6a29db2
virtualDev <antrostuff@gmail.com>
2017-11-08 17:24:01 +0300

    1207

---
+++ task12/task1207/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    static void print(int i)
+    {
+
+    }
+
+    static void print(Integer i)
+    {
+
+    }
+
+    //Напишите тут ваши методы
+}
