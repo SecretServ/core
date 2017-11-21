443f0892eb9cdc7eaa8826b25eaf8fe4b5662a60
virtualDev <antrostuff@gmail.com>
2017-11-21 16:43:53 +0300

    1510

--- task15/task1504/Solution.java
+++ task15/task1504/Solution.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        List<Book> books = new LinkedList<Book>();
+        List<Book> books = new LinkedList<>();
         books.add(new MarkTwainBook("Tom Sawyer"));
         books.add(new AgathaChristieBook("Hercule Poirot"));
         System.out.println(books);
@@ -17,44 +17,63 @@ public class Solution {
 
     abstract static class Book {
         private String author;
-
         public Book(String author) {
             this.author = author;
         }
-
         public abstract Book getBook();
-
         public abstract String getTitle();
-
         private String getOutputByBookType() {
             String agathaChristieOutput = author + ", " + getBook().getTitle() + " is a detective";
             String markTwainOutput = getBook().getTitle() + " book was written by " + author;
 
             String output = "output";
             //Add your code here
+            if (this instanceof AgathaChristieBook ){
+                output = agathaChristieOutput;
+            } else if (this instanceof MarkTwainBook){
+                output = markTwainOutput;
+            }
 
             return output;
         }
 
-        public static class MarkTwainBook extends Book {
-            private String title;
 
-            public MarkTwainBook(){
-                super("Mark Twain");
-            }
-
-            public MarkTwainBook(String title){
-                this.title = title;
-            }
-
-            public MarkTwainBook getBook(){
-                return this.title;
-            }
-
-        }
 
         public String toString() {
             return getOutputByBookType();
+        }
+    }
+
+    public static class MarkTwainBook extends Book {
+        public String title;
+
+        public MarkTwainBook(String str) {
+            super("Mark Twain");
+            title = str;
+        }
+
+        public MarkTwainBook getBook(){
+            return this;
+        }
+        public String getTitle(){
+            return title;
+        }
+    }
+
+    public static class AgathaChristieBook extends Book {
+        public String title;
+        public AgathaChristieBook(String str){
+            super("Agatha Christie");
+            title = str;
+        }
+        /*public AgathaChristieBook(){
+            super("Agatha Christie");
+        }*/
+        public AgathaChristieBook getBook(){
+            return this;
+        }
+        public String getTitle(){
+            return title;
         }
     }
 }
