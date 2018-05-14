5f22fa1d66c7eb8182c22eab73fa40749363c9a5
SecretServ <antrobill@gmail.com>
2018-05-14 17:01:09 +0300

    1911

---
+++ task19/task1913/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task19.task1913;
+
+/* 
+Выводим только цифры
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+
+        System.setOut(consoleOut);
+
+        String regex = "\\D";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(result);
+
+        if(m.find()) {
+            result = result.replaceAll(regex, "");
+        }
+
+        System.out.print(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
---
+++ task19/task1914/Solution.java
@@ -0,0 +1,59 @@
+package com.javarush.task.task19.task1914;
+
+/* 
+Решаем пример
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+        System.setOut(consoleStream); //return original output stream
+
+        String regex1 = "\\d";
+        String regex2 = "[\\+\\-\\*]";
+        String sign = null;
+        Pattern p1 = Pattern.compile(regex1);
+        Pattern p2 = Pattern.compile(regex2);
+        Matcher m1 = p1.matcher(result);
+        Matcher m2 = p2.matcher(result);
+
+        while(m1.find()) {
+            list.add(m1.group());
+        }
+
+        if (m2.find()) {
+            list.add(m2.group());
+        }
+
+        int target = 0;
+
+        switch (list.get(2)) {
+            case "+" : target = ////////// continue from here
+        }
+
+        int target = Integer.parseInt(list.get(0)) + Byte.parseByte(list.get(2)) + Integer.parseInt(list.get(1));
+
+        System.out.println(list.get(0) + " " + list.get(2) + " " + list.get(1) + " = " + target);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("3 * 6 = ");
+        }
+    }
+}
+
